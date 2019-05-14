9013fb0bb893d344bdba55a4d812b2dc208d3247
rrosztoczy <rrosztoczy@gmail.com>
2019-05-14 18:07:20 -0400

    updated request specs

--- spec/requests/activity_memories_spec.rb
+++ spec/requests/activity_memories_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 # TODO:Finish setting up tests to use randomized faker bot data instead of seed data.
-# Need to create the associated emotion, thought etc. data and link them to the thoughts.
+# Need to create the associated emotion, thought etc. data and link them to the activities.
 # Also need to make sure tests account for serializer etc.
 
-RSpec.describe 'Thoughts API', type: :request do
+RSpec.describe 'ActivityMemory API', type: :request do
   # initialize test data 
-#   let!(:thoughts) { create_list(:thought, 10) }
-  let(:thought_id) { Thought.first.id }
+#   let!(:activities) { create_list(:activity, 10) }
+  let(:activity_memory_id) { ActivityMemory.first.id }
 
-  # Test suite for GET /thoughts
-  describe 'GET /thoughts' do
+  # Test suite for GET /activity_memory
+  describe 'GET /activity_memory' do
     # make HTTP get request before each example
-    before { get '/thoughts' }
+    before { get '/activity_memory' }
 
-    it 'returns thoughts' do
+    it 'returns activity_memories' do
       # Note `json` is a custom helper to parse JSON responses
       expect(json).not_to be_empty
     end
@@ -23,14 +23,14 @@ RSpec.describe 'Thoughts API', type: :request do
     end
   end
 
-  # Test suite for GET /thoughts/:id
-  describe 'GET /thoughts/:id' do
-    before { get "/thoughts/#{thought_id}" }
+  # Test suite for GET /activity_memories/:id
+  describe 'GET /activity_memories/:id' do
+    before { get "/activity_memories/#{activity_memory_id}" }
 
     context 'when the record exists' do
-      it 'returns the thought' do
+      it 'returns the activity_memory' do
         expect(json).not_to be_empty
-        expect(json['id']).to eq(thought_id)
+        expect(json['id']).to eq(activity_memory_id)
       end
 
       it 'returns status code 200' do
@@ -39,29 +39,29 @@ RSpec.describe 'Thoughts API', type: :request do
     end
 
     context 'when the record does not exist' do
-      let(:thought_id) { 100 }
+      let(:activity_memory_id) { 100 }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
 
       it 'returns a not found message' do
-        expect(response.body).to include("Couldn't find Thought")
+        expect(response.body).to match("Couldn't find ActivityMemory")
       end
     end
   end
 
   # 
-#   TODO:Do I need to come back to this and create the tables through thoughts?
-# Test suite for POST /thoughts
-#   describe 'POST /thoughts' do
+#   TODO:Do I need to come back to this and create the tables through activities?
+# Test suite for POST /activity_memory
+#   describe 'POST /activity_memory' do
 #     # valid payload
 #     let(:valid_attributes) { { title: 'Learn Elm', created_by: '1' } }
 
 #     context 'when the request is valid' do
-#       before { post '/thoughts', params: valid_attributes }
+#       before { post '/activity_memory', params: valid_attributes }
 
-#       it 'creates a thought' do
+#       it 'creates a activity' do
 #         expect(json['title']).to eq('Learn Elm')
 #       end
 
@@ -71,7 +71,7 @@ RSpec.describe 'Thoughts API', type: :request do
 #     end
 
 #     context 'when the request is invalid' do
-#       before { post '/thoughts', params: { title: 'Foobar' } }
+#       before { post '/activity_memory', params: { title: 'Foobar' } }
 
 #       it 'returns status code 422' do
 #         expect(response).to have_http_status(422)
@@ -84,12 +84,12 @@ RSpec.describe 'Thoughts API', type: :request do
 #     end
 #   end
 
-#   # Test suite for PUT /thoughts/:id
-#   describe 'PUT /thoughts/:id' do
+#   # Test suite for PUT /activity_memory/:id
+#   describe 'PUT /activity_memory/:id' do
 #     let(:valid_attributes) { { title: 'Shopping' } }
 
 #     context 'when the record exists' do
-#       before { put "/thoughts/#{thought_id}", params: valid_attributes }
+#       before { put "/activity_memory/#{activity_id}", params: valid_attributes }
 
 #       it 'updates the record' do
 #         expect(response.body).to be_empty
@@ -101,9 +101,9 @@ RSpec.describe 'Thoughts API', type: :request do
 #     end
 #   end
 
-  # Test suite for DELETE /thoughts/:id
-  describe 'DELETE /thoughts/:id' do
-    before { delete "/thoughts/#{thought_id}" }
+  # Test suite for DELETE /activity_memory/:id
+  describe 'DELETE /activity_memories/:id' do
+    before { delete "/activity_memories/#{activity_memory_id}" }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
