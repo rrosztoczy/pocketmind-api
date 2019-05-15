15f7ba97c068b5b4e633f1b056baecdc8c0a5ef3
rrosztoczy <rrosztoczy@gmail.com>
2019-05-15 11:50:35 -0400

    made test updates

--- spec/models/emotion_spec.rb
+++ spec/models/emotion_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Emotion, type: :model do
-  it { should belong_to(:memory) }
+  it { should has_many(:emotion_memories) }
   it { should validate_presence_of(:feeling, :intensity, :stress_level) }
 end
