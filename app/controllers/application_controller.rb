599e403a5b9616007876311360163497b87ccc7d
rrosztoczy <rrosztoczy@gmail.com>
2019-05-21 18:03:38 -0400

    hopefully done with auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
     include Response
     include ExceptionHandler
     before_action :authorized
+    # helper_method :current_user
 
     def encode_token(payload)
         JWT.encode(payload, ENV["jwt_secret"])
@@ -13,12 +14,9 @@ class ApplicationController < ActionController::API
     end
 
     def decoded_token
-        # puts `in token decoding`
-        puts "tyring to decode"
         if auth_header
             # puts `in auth header`
             token = auth_header.split(' ')[1]
-            puts "#{token}"
             begin
                 JWT.decode(token, ENV["jwt_secret"], true, algorithm: 'HS256')
             rescue JWT::DecodeError
@@ -29,10 +27,9 @@ class ApplicationController < ActionController::API
 
     def current_user
         if decoded_token
-            puts "token decoded"
           user_id = decoded_token[0]['user_id']
-        #   puts `user id is #{user.id}`
           @user = User.find_by(id: user_id)
+        #   puts "found user #{user_id}"
         end
       end
     
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,4 +1,5 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
   attributes :email, :first_name, :last_name
   # Todo: update this if you need to go through it for authing data
+  has_many :memories, each_serializer: MemorySerializer
 end
