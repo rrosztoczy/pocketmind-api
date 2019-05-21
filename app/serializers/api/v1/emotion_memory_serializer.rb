d5ab7507309ddca72970e118060407c1736f0db9
rrosztoczy <rrosztoczy@gmail.com>
2019-05-21 09:08:13 -0400

    almost done with emotion memories

--- app/serializers/api/v1/emotion_memory_serializer.rb
+++ app/serializers/api/v1/emotion_memory_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::EmotionMemorySerializer < ActiveModel::Serializer
-  attributes :id, :emotion, :intensity, :pleasure, :memory
+  attributes :id, :emotion, :intensity, :pleasure, :memory, :created_at
   # TODO: Update so that emotion: is same level as memory, only shows relevant info, shows user
 end
