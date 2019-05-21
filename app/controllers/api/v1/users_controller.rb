32f1340a396998dc8d18d140477a01f84f7f6f7e
rrosztoczy <rrosztoczy@gmail.com>
2019-05-21 18:16:10 -0400

    still working on auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
         before_action :set_user, only: [:show, :update, :destroy]
-        skip_before_action :authorized, only: [:profile, :index, :create, :new]
+        skip_before_action :authorized, only: [:create, :new]
     
         # GET /users
         def index
