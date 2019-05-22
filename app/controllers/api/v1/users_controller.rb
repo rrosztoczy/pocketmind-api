1b7d096b32461110280d72b33d5609dd9e1e5551
rrosztoczy <rrosztoczy@gmail.com>
2019-05-21 23:15:47 -0400

    can also create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -22,8 +22,8 @@ class Api::V1::UsersController < ApplicationController
         def create
           @user = User.create(user_params)
           if @user.valid?
-            @token = encode_token(user_id: @user.id)
-            render json: { user: Api::V1::UserSerializer.new(@user), jwt: @token }, status: :created
+            token = encode_token(user_id: @user.id)
+            render json: {email: @user.email, jwt: token }, status: :created
           else
             render json: { error: 'failed to create user' }, status: :not_acceptable
           end
