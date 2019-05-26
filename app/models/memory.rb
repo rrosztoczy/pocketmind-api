d833aeeef58b8428d8ed981f1e469de7976c644b
rrosztoczy <rrosztoczy@gmail.com>
2019-05-26 17:30:45 -0400

    updated memory model

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -2,6 +2,7 @@ class Memory < ApplicationRecord
   belongs_to :user
   has_many :emotion_memories, dependent: :destroy
   has_many :thought_memories, dependent: :destroy
-  accepts_nested_attributes_for :emotion_memories, :thought_memories
+  has_many :activity_memories, dependent: :destroy
+  accepts_nested_attributes_for :emotion_memories, :thought_memories, :activity_memories
   validates_presence_of :user_id
 end
